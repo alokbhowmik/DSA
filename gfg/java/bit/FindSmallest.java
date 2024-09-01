7ee85b5a0524d1b79a2940e6307849db14db2240
Alok Bhowmik <alokbhowmic01@gmail.com>
2024-09-01 20:31:00 +0530

    GFT Weekly Coding Contest 170 Question 1

---
+++ gfg/java/bit/FindSmallest.java
@@ -0,0 +1,24 @@
+/**
+ * Problem Name : Find Smallest 
+ * Problem Link : https://practice.geeksforgeeks.org/contest/gfg-weekly-170-rated-contest/problems
+ * 
+ * Not able to copy the problem statement. Kindly check the above Lin
+
+ * 
+ * Time Complexity : O(64) 
+ * Space Complexity : O(1)
+ * 
+ */
+class FindSmallest {
+
+    public static int getSmallest(int n) {
+        // code here
+        for(int i=0; i<64; i++){
+            int val = (1 << i) & n;
+            if(val== 0){
+                return 1<<i;
+            }
+        }
+        return -1;
+    }
+}
