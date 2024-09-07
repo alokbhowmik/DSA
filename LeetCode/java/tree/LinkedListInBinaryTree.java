f398550672dff480fd45af0bdcd45a80e7b03702
Alok Bhowmik <alokbhowmic01@gmail.com>
2024-09-07 08:34:45 +0530

    Today's 07.09.2024 Leetcode Problem LinkedList In Binary Tree

---
+++ LeetCode/java/tree/LinkedListInBinaryTree.java
@@ -0,0 +1,41 @@
+package LeetCode.java.tree;
+
+public class LinkedListInBinaryTree {
+     public class ListNode {
+             int val;
+             ListNode next;
+             ListNode() {}
+             ListNode(int val) { this.val = val; }
+             ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+         }
+        
+       // Definition for a binary tree node.
+         public class TreeNode {
+             int val;
+             TreeNode left;
+             TreeNode right;
+             TreeNode() {}
+             TreeNode(int val) { this.val = val; }
+             TreeNode(int val, TreeNode left, TreeNode right) {
+                 this.val = val;
+                 this.left = left;
+                 this.right = right;
+             }
+         }
+        
+    boolean dfs(ListNode head, ListNode curr, TreeNode root){
+        if(curr == null) return true;
+        if(root == null) return false;
+        if(curr.val == root.val) curr = curr.next;
+        else if(head.val == root.val) head = head.next;
+        else{
+            curr = head;
+        }
+        boolean left = dfs(head, curr, root.left);
+        boolean right = dfs(head, curr, root.right);
+        return left || right;
+    }
+    public boolean isSubPath(ListNode head, TreeNode root) {
+       return dfs(head, head, root);
+    }
+}
