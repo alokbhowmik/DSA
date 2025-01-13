4e884f5c5746474a5400d7256103a612936a2cea
Alok Bhowmik <alokbhowmic01@gmail.com>
2025-01-13 07:59:26 +0530

    13.01.2025 GFG Problem Container With Most Water

---
+++ gfg/java/array/ContainerWithMostWater.java
@@ -0,0 +1,43 @@
+package array;
+/*
+*
+* Problem Name : Container With Most Water
+*
+* Problem Link : https://www.geeksforgeeks.org/problems/container-with-most-water0535/1
+*
+* Problem Statement
+*
+* Given an array arr[] of non-negative integers, where each element arr[i] represents the height of the vertical lines, find the maximum amount of water that can be contained between any two lines, together with the x-axis.
+
+Note: In the case of a single vertical line it will not be able to hold water.
+
+Examples:
+
+Input: arr[] = [1, 5, 4, 3]
+Output: 6
+Explanation: 5 and 3 are 2 distance apart. So the size of the base is 2. Height of container = min(5, 3) = 3. So, total area to hold water = 3 * 2 = 6.
+Input: arr[] = [3, 1, 2, 4, 5]
+Output: 12
+Explanation: 5 and 3 are 4 distance apart. So the size of the base is 4. Height of container = min(5, 3) = 3. So, total area to hold water = 4 * 3 = 12.
+Input: arr[] = [2, 1, 8, 6, 4, 6, 5, 5]
+Output: 25
+Explanation: 8 and 5 are 5 distance apart. So the size of the base is 5. Height of container = min(8, 5) = 5. So, the total area to hold water = 5 * 5 = 25.
+Constraints:
+1<= arr.size() <=10^5
+1<= arr[i] <=10^4
+ * */
+public class ContainerWithMostWater {
+    public int maxWater(int arr[]) {
+        // Code Here
+        // TC = O(n) SC = O(1)
+        int ans = 0;
+        int l = 0, r = arr.length - 1;
+        while(l < r){
+            int minHeight = Math.min(arr[l], arr[r]);
+            ans = Math.max(ans, (r - l) * minHeight);
+            if(arr[l] < arr[r]) l++;
+            else r--;
+        }
+        return ans;
+    }
+}
