5b30dafce6c1a0620bfda501c02f76f5cb723fd4
Alok Bhowmik <alokbhowmic01@gmail.com>
2025-01-29 08:54:24 +0530

    28.01.2025 GFG Problem Implement Pow

---
+++ gfg/java/math/ImplementPow.java
@@ -0,0 +1,31 @@
+package math;
+/*
+* Problem Name : Implement Pow
+*
+* Problem Link : https://www.geeksforgeeks.org/problems/powx-n/1
+*
+* Problem Statement
+*
+* Implement the function power(b, e), which calculates b raised to the power of e (i.e. be).
+
+Examples:
+
+Input: b = 3.00000, e = 5
+Output: 243.00000
+Input: b = 0.55000, e = 3
+Output: 0.16638
+Input: b = -0.67000, e = -7
+Output: -16.49971
+Constraints:
+
+-100.0 < b < 100.0
+-109 <= e <= 109
+Either b is not zero or e > 0.
+-104 <= be <= 104
+* */
+public class ImplementPow {
+    double power(double b, int e) {
+        // code here
+        return Math.pow(b, e);
+    }
+}
