b03f73821fdb8d3b1ae34b5f32b206ff76bf8606
Alok Bhowmik <alokbhowmic01@gmail.com>
2025-02-28 08:34:13 +0530

    28.02.2025 LeetCode  Problem Shortest Common Supersequence

--- LeetCode/java/dp/ShortestCommonSupersequence.java
+++ LeetCode/java/dp/ShortestCommonSupersequence.java
@@ -36,6 +36,11 @@ str1 and str2 consist of lowercase English letters.
 *--------------
 *TC = O(2^n) SC = O(2^n)  // due to internal stack
 * TC = (n + m) SC = (m *n) // due to memorization
+*
+* Approach 2
+* --------------
+*
+* TC = O(m * n ) SC = O(m * n )
 */
 public class ShortestCommonSupersequence{
     private Map<String, String> map;
@@ -70,7 +75,50 @@ public class ShortestCommonSupersequence{
     }
 
     public String shortestCommonSupersequence(String str1, String str2) {
-        map = new HashMap<>();
-        return solve(str1, str2);
+        int n = str1.length();
+        int m = str2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int row = 0; row <= n; row++) {
+            dp[row][0] = row;
+        }
+        for (int col = 0; col <= m; col++) {
+            dp[0][col] = col;
+        }
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= m; col++) {
+                if (str1.charAt(row - 1) == str2.charAt(col - 1)) {
+                    dp[row][col] = dp[row - 1][col - 1] + 1;
+                } else {
+                    dp[row][col] = Math.min(dp[row - 1][col], dp[row][col - 1]) + 1;
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        int row = n, col = m;
+        while(row > 0 && col > 0){
+            if(str1.charAt(row - 1) == str2.charAt(col - 1)){
+                sb.append(str1.charAt(row - 1));
+                row--;
+                col--;
+            }else if(dp[row - 1][col] < dp[row][col - 1]){
+                sb.append(str1.charAt(row - 1));
+                row--;
+            }else{
+                sb.append(str2.charAt(col - 1));
+                col--;
+            }
+        }
+
+        while(row > 0){
+            sb.append(str1.charAt(row - 1));
+            row--;
+        }
+
+        while(col > 0){
+            sb.append(str2.charAt(col - 1));
+            col--;
+        }
+        return sb.reverse().toString();
+
     }
 }
