a38a6db4c379a8644309bba66d8a01c23861e981
Alok Bhowmik <alokbhowmic01@gmail.com>
2025-03-12 08:11:51 +0530

    11.03.2025 GFG Problem Min Cost Climbing Stairs Approach 2

--- gfg/java/dp/MinCostClimbingStairs.java
+++ gfg/java/dp/MinCostClimbingStairs.java
@@ -10,21 +10,26 @@ package dp;
 * Approach 1 : Recursion
 *--------------
 *TC = O(2^n) SC = O(1)
+*
+* Approach 2 : Recursion + Memorization
+ *--------------
+ *TC = O(n) SC = O(n)
 */
 public class MinCostClimbingStairs{
     int minCostClimbingStairs(int[] cost) {
+        Integer[] dp = new Integer[cost.length + 1];
         return Math.min(
-                solve(0, cost.length, cost),
-                solve(1, cost.length, cost)
+                solve(0, cost.length, cost, dp),
+                solve(1, cost.length, cost, dp)
         );
     }
 
-    private int solve(int i, int n, int[] cost){
+    private int solve(int i, int n, int[] cost, Integer[] dp){
         if(i > n) return 1000;
         if(i == n) return 0 ;
         return cost[i] + Math.min(
-                solve(i + 1, n, cost),
-                solve(i + 2, n, cost)
+                solve(i + 1, n, cost, dp),
+                solve(i + 2, n, cost, dp)
         );
     }
 }
