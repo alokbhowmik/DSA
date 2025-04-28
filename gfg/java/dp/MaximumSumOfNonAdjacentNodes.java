806c051f965a6a78ef6ecf23bb92df8f8390f2ad
Alok Bhowmik <alokbhowmic01@gmail.com>
2025-04-28 19:34:58 +0530

    28.04.2025 GFG Problem Maximum sum of Non-adjacent nodes Approach 1

---
+++ gfg/java/dp/MaximumSumOfNonAdjacentNodes.java
@@ -0,0 +1,64 @@
+package dp;
+
+/*
+*Problem Name : Maximum sum of Non adjacent nodes
+*
+*Problem Link : https://www.geeksforgeeks.org/problems/maximum-sum-of-non-adjacent-nodes/1
+*
+*Problem Statement : 
+*
+* Given a binary tree with a value associated with each node. Your task is to select a subset of nodes such that the sum of their values is maximized, with the condition that no two selected nodes are directly connected that is, if a node is included in the subset, neither its parent nor its children can be included.
+
+Examples:
+
+Input: root[] = [11, 1, 2]
+
+Output: 11
+Explanation: The maximum sum is obtained by selecting the node 11.
+
+Input: root[] = [1, 2, 3, 4, N, 5, 6]
+
+Output: 16
+Explanation: The maximum sum is obtained by selecting the nodes 1, 4, 5, and 6, which are not directly connected to each other. Their total sum is 16.
+
+Constraints:
+1 ≤ no. of nodes in the tree ≤ 104
+1 ≤ Node.val ≤ 10^5
+
+*
+* Approach 1 
+*--------------
+*TC = O(2^n) SC = O(1)
+*/
+public class MaximumSumOfNonAdjacentNodes{
+    private class Node
+    {
+        int data;
+        Node left, right;
+
+        Node(int data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public int getMaxSum(Node root) {
+        return solve(root);
+    }
+    private int solve(Node root){
+        if(root == null) return 0 ;
+
+        int take = root.data;
+        if(root.left != null){
+            take += solve(root.left.left);
+            take += solve(root.left.right);
+        }
+        if(root.right != null){
+            take += solve(root.right.left);
+            take += solve(root.right.right);
+        }
+        int skip = solve(root.left) + solve(root.right);
+        return Math.max(take, skip);
+    }
+}
